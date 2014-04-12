14474cde27a4d261fbf639703bf2e83dd82ad2fe
R.Lalremruata <remralte@gmail.com>
2014-04-12 12:20:00 +0530

    admin auth

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -3,6 +3,9 @@ class AdminController extends BaseController
 {
 	public function getIndex()
 	{
+		if(in_array(Auth::user()->role, array('editor','user')))
+			return Redirect::to('denied');
+	
 		return View::make('admin.index');
 	}
 }
